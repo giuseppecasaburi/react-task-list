ece799d43abefefc83d36d2acac405c115b20770
giuseppecasaburi <enzobiondi0000@libero.it>
2024-12-16 21:42:28 +0100

    feat: bonus

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Mostriamo in due liste diverse gli elementi dell’array *tasks*:
 - Nella prima lista mostriamo solo i task (title, priority e estimatedTime) che hanno stato “backlog” o “in_progress”; ✔
 - Nella seconda lista mostriamo i task che hanno stato “completed”. ✔
-Infine riprodurre il layout lasciato in allegato. 
+Infine riprodurre il layout lasciato in allegato. ✔
 
 **BONUS**
 Mostrare lo stato di ogni task in un piccolo tag di fianco al titolo, associando però ad ogni stato un colore diverso.
--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,47 @@ import taskArray from "../tasks.js";
 
 function App() {
 
-  const inProgress = taskArray.filter(currItem => currItem.state === "backlog" || currItem.state === "in_progress"); 
-  
+  const inProgress = taskArray.filter(currItem => currItem.state === "backlog" || currItem.state === "in_progress");
+
   const completed = taskArray.filter(currItem => currItem.state === "completed");
 
   const printList = (array) => {
-    return array.map((currItem) => 
-      (<li key={currItem.id} className="style-none">
-        <div><strong>{currItem.title}</strong></div>
-        <div>Priorità: {currItem.priority}</div>
-        <div>Tempo stimato: {currItem.estimatedTime}</div>
-      </li>))
+    return array.map((currItem) =>
+    (<li key={currItem.id} className="style-none">
+      <div className="title-elem"><strong>{currItem.title}</strong></div>
+      <span className={`state ${currItem.state}`}>{currItem.state}</span>
+      <div className="elem">Priorità: {currItem.priority}</div>
+      <div className="elem">Tempo stimato: {currItem.estimatedTime}</div>
+    </li>))
   }
-  
-  
+
+
   return (
     <>
-    <h1>Task Manager</h1>
-    <h2>Current task {inProgress.length}</h2>
-    <ul>
-      {printList(inProgress)}
-    </ul>
-    <h2>Completed task {completed.length}</h2>
-    <ul>
-      {printList(completed)}
-    </ul>
+      <header className="header">
+        <h1 className="container">Task Manager</h1>
+      </header>
+      
+      <main>
+        
+      <div className="container">
+        <section className="current-task">
+          <h2 className="curr-task">Current task {inProgress.length}</h2>
+          <ul>
+            {printList(inProgress)}
+          </ul>
+        </section>
+        
+        <hr />
+        <section className="completed-task">
+          <h2 className="completed-task">Completed task {completed.length}</h2>
+          <ul>
+            {printList(completed)}
+          </ul>
+        </section>
+      </div>
+
+      </main>
     </>
   )
 }
